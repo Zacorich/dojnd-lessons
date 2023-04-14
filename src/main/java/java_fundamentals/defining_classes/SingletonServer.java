a10ab94b3c0995d047106c296e7bf37f52cb0bd3
Constantin Zagorsky <zacorich@gmail.com>
2023-04-14 15:57:11 +0100

    A better Singleton implementation reference added

--- src/main/java/java_fundamentals/defining_classes/SingletonServer.java
+++ src/main/java/java_fundamentals/defining_classes/SingletonServer.java
@@ -3,15 +3,24 @@ package java_fundamentals.defining_classes;
 import java.util.ArrayList;
 
 public class SingletonServer {
+    //here is a better alternative of Singleton creation -https://www.digitalocean.com/community/tutorials/thread-safety-in-java-singleton-classes
+    //to achieve following features:
+    //1. Thread safety is guaranteed
+    //2. Client application can pass arguments
+    //3. Lazy initialization achieved
+    //4. Synchronization overhead is minimal and applicable only for first few threads when the variable is null.
+
     //use volatile jave keyword for thread safety - https://stackoverflow.com/a/11640026/4592525
+    //this is a generic singleton and it is not thread safe
     private static volatile SingletonServer instance = new SingletonServer();
     private ArrayList hotelRoomsAsSomeSharedObjectBetweenClasses;
-    private SingletonServer(){
+
+    private SingletonServer() {
         //initialise shared object
         hotelRoomsAsSomeSharedObjectBetweenClasses = new ArrayList();
     }
 
-    public static SingletonServer getInstance(){
+    public static SingletonServer getInstance() {
         return instance;
     }
 
