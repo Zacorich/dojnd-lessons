74fd6655f482ffb81c92c91c9566875733859d32
Constantin Zagorsky <zacorich@gmail.com>
2023-04-14 14:52:11 +0100

    some precision added to AccessModifiers

--- src/main/java/java_fundamentals/AccessModifiers.java
+++ src/main/java/java_fundamentals/AccessModifiers.java
@@ -3,6 +3,6 @@ package java_fundamentals;
 public class AccessModifiers {
     //private - defining class only
     //default - defining class, same package classes
-    //protected - defining class, same package classes, subclasses
-    //public - defining class, same package classes, subclasses, classes from other packages
+    //protected - defining class, same package classes, subclasses( if they lie in other packages)
+    //public - defining class, same package classes, subclasses( if they lie in other packages), any classes from other packages
 }
