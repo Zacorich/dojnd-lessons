cab756802dcbf22b8aa9f27da0767c18b6222cf1
Constantin Zagorsky <zacorich@gmail.com>
2023-04-14 19:13:24 +0100

    + Default constructors are created automatically if not defined explicitly

---
+++ src/main/java/java_fundamentals/defining_classes/DefaultConstructor.java
@@ -0,0 +1,9 @@
+package java_fundamentals.defining_classes;
+
+public class DefaultConstructor {
+    public int a;
+
+    public static void main(String[] args) {
+        DefaultConstructor z = new DefaultConstructor();
+    }
+}
