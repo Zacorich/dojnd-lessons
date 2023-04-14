0bdf4d00499531161d531996ce331086ea568f67
Constantin Zagorsky <zacorich@gmail.com>
2023-04-14 16:05:28 +0100

    Some refactoring

--- src/main/java/java_fundamentals/defining_classes/PersonTester.java
+++ src/main/java/java_fundamentals/defining_classes/PersonTester.java
@@ -6,5 +6,9 @@ public class PersonTester {
         Person mike = new Person("Mike", "Lipson");
         System.out.println(bob.toString());
         System.out.println(mike.toString());
+
+        //don't need explicitly call toString() method on objects as println does that for us
+        System.out.println(bob);
+        System.out.println(mike);
     }
 }
