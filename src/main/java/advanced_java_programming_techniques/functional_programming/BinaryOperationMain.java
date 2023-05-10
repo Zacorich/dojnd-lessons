2c4926c1619c6d63dbac972f35629b1871b33861
Constantin Zagorsky <zacorich@gmail.com>
2023-05-10 01:19:31 +0100

    + Exercise: Defining a Functional Interface

--- src/main/java/advanced_java_programming_techniques/functional_programming/BinaryOperationMain.java
+++ src/main/java/advanced_java_programming_techniques/functional_programming/BinaryOperationMain.java
@@ -1,8 +1,14 @@
 package advanced_java_programming_techniques.functional_programming;
 
+import java.util.function.BinaryOperator;
+
 public class BinaryOperationMain {
     public static void main(String[] args) {
         BinaryOperation add = (a, b) -> a + b;
         assert 5 == add.apply(3,2);
+
+        //using build int BinaryOperator
+        BinaryOperator<Integer> add2 = (a, b) -> a + b;
+        System.out.println(add2.apply(10, 10));
     }
 }
