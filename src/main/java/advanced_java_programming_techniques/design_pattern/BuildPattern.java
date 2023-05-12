1e7747bb3e6100a17fbbf3ad32207683ed6e1735
Constantin Zagorsky <zacorich@gmail.com>
2023-05-12 18:09:31 +0100

    + Builder design patter

---
+++ src/main/java/advanced_java_programming_techniques/design_pattern/BuildPattern.java
@@ -0,0 +1,23 @@
+package advanced_java_programming_techniques.design_pattern;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class BuildPattern {
+    public static void main(String[] args) {
+        UdacisearchClient client =
+                new UdacisearchClient.Builder()
+                        .setName("CatFacts LLC")
+                        .setId(17)
+                        .setQuarterlyBudget(8000)
+                        .setNumEmployees(5)
+                        .setContractStart(Instant.now())
+                        .setContractLength(Duration.ofDays(180))
+                        .setTimeZone(ZoneId.of("America/Los_Angeles"))
+                        .setBillingAddress("555 Meowmers Ln, Riverside, CA 92501")
+                        .build();
+
+        System.out.println(client);
+    }
+}
