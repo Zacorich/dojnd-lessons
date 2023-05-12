9fa2baa0c62be1467ac5c2494a941c26083426e9
Constantin Zagorsky <zacorich@gmail.com>
2023-05-12 12:28:51 +0100

    + use try with resources to close the BufferedWriter too

--- src/main/java/advanced_java_programming_techniques/introduction_to_files_and_io/words_merging/MakeShards.java
+++ src/main/java/advanced_java_programming_techniques/introduction_to_files_and_io/words_merging/MakeShards.java
@@ -33,48 +33,48 @@ public final class MakeShards {
         //       skip any words. Write the shard files in the newly created "outputFolder", using the
         //       getOutputFileName(int) method to name the individual shard files.
 
-        BufferedReader br = Files.newBufferedReader(input, StandardCharsets.UTF_8);
-        String line = null;
-        List<String> singleShardWordsCollection = new LinkedList<>();
-        int currentShardId = 0;
-        while((line = br.readLine()) != null){
-            System.out.println(line);
-            singleShardWordsCollection.add(line);
-            if(singleShardWordsCollection.size() == SHARD_SIZE){
-                //sort words in current shard and write to file using buffered writer
-                Path currentShardPath = outputFolder.resolve(getOutputFileName(currentShardId)).normalize();
-                BufferedWriter bw =
-                        Files.newBufferedWriter(currentShardPath,
-                                StandardCharsets.UTF_8);
-                int[] index = { 0 };
-                singleShardWordsCollection.stream().sorted().collect(Collectors.toList()).forEach(l -> {
-                    try {
-                        //capture array with one element to store and increment current line index
-                        index[0]++;
-                        bw.write(l);
-                        //write new line after all words except for the the last word
-                        if(index[0] < SHARD_SIZE) {
-                            bw.newLine();
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+        try (BufferedReader br = Files.newBufferedReader(input)) {
+            String line = null;
+            List<String> singleShardWordsCollection = new LinkedList<>();
+            int currentShardId = 0;
+            while ((line = br.readLine()) != null) {
+                singleShardWordsCollection.add(line);
+                if (singleShardWordsCollection.size() == SHARD_SIZE) {
+                    //sort words in current shard and write to file using buffered writer
+                    Path currentShardPath = outputFolder.resolve(getOutputFileName(currentShardId)).normalize();
+                    try (BufferedWriter bw =
+                                 Files.newBufferedWriter(currentShardPath,
+                                         StandardCharsets.UTF_8)) {
+                        int[] index = {0};
+                        singleShardWordsCollection.stream().sorted().collect(Collectors.toList()).forEach(l -> {
+                            try {
+                                //capture array with one element to store and increment current line index
+                                index[0]++;
+                                bw.write(l);
+                                //write new line after all words except for the last word
+                                if (index[0] < SHARD_SIZE) {
+                                    bw.newLine();
+                                }
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
                     }
-                });
-                bw.close();
 
-                //clear the set to be used for the next shard
-                singleShardWordsCollection.clear();
-                currentShardId++;
+                    //clear the set to be used for the next shard
+                    singleShardWordsCollection.clear();
+                    currentShardId++;
+                }
             }
         }
-        deleteShardsFolderWithFiles(outputFolder);
+        //deleteShardsFolderWithFiles(outputFolder);
     }
 
-    private static void deleteShardsFolderWithFiles(Path outputFolder){
+    private static void deleteShardsFolderWithFiles(Path outputFolder) {
         try {
             Files.walk(outputFolder).forEach(f -> {
                 try {
-                    if(!Files.isDirectory(f)) {
+                    if (!Files.isDirectory(f)) {
                         Files.deleteIfExists(f);
                     }
                 } catch (IOException e) {
