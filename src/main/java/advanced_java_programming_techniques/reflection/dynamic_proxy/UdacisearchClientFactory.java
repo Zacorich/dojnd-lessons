acc696c7f7c87c03a4088187597da697e1820fe4
Constantin Zagorsky <zacorich@gmail.com>
2023-08-06 16:01:24 +0100

    3.5.11 - todo list completed

--- src/main/java/advanced_java_programming_techniques/reflection/dynamic_proxy/UdacisearchClientFactory.java
+++ src/main/java/advanced_java_programming_techniques/reflection/dynamic_proxy/UdacisearchClientFactory.java
@@ -1,6 +1,7 @@
 package advanced_java_programming_techniques.reflection.dynamic_proxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
@@ -33,8 +34,25 @@ public final class UdacisearchClientFactory {
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             // TODO: Fill this method in!
-
-            return null;
+            String methodName = method.getName();
+            if (method.getDeclaringClass().equals(Object.class)) {
+                // Invoke toString() and hashCode() directly on the property Map.
+                try {
+                    return method.invoke(properties, args);
+                } catch (InvocationTargetException e) {
+                    throw e.getTargetException();
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            String propertyName = getPropertyName(methodName);
+            if(propertyName.contentEquals("")){
+                throw new RuntimeException("Method is not a property getter: " + methodName);
+            }
+            if(!properties.containsKey(propertyName)){
+                throw new RuntimeException("No property named \"" + propertyName + "\" found in map.");
+            }
+            return properties.get(propertyName);
         }
     }
 
